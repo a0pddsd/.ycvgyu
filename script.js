e211b15fc814ceef95de75a0ef474c4b08a44954
Elayt <elaytpatters@gmail.com>
2022-04-17 20:43:07 +0500

    .

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let inputPoints = document.querySelector("#points");
 let grantBall = 100;
 let kontraktBall = 75;
 let SupKontBall = grantBall / 2;
+let failBall = grantBall / 4;
 
 outMessage.innerHTML = "";
 
@@ -30,5 +31,33 @@ checkBtn.addEventListener("click", function () {
     inputPoints.placeholder = "Enter Your Points";
     inputPoints.style.boxShadow = "1px 1px 3px 1px 	#70707033";
   }
+
+  if (numbPoints >= grantBall) {
+    outTitle.classList.remove("d-none");
+    outTitle.classList.add("out-title");
+    outTitle.innerHTML = "Congratulations";
+    outMessage.classList.add("out-message");
+    outMessage.innerHTML = `You entered ${select} on a Grant basis with ${numbPoints} points`;
+  } else if (numbPoints >= kontraktBall && numbPoints < grantBall) {
+    outTitle.classList.remove("d-none");
+    outTitle.classList.add("out-warning-title");
+    outTitle.innerHTML = "Congratulations";
+    outMessage.classList.add("out-warning-message");
+    outMessage.innerHTML = `You entered ${select} on a Contract basis with ${numbPoints} points`;
+  } else if (numbPoints <= SupKontBall && numbPoints < kontraktBall) {
+    outTitle.classList.remove("d-none");
+    outTitle.classList.add("out-warning-title");
+    outTitle.innerHTML = "Congratulations";
+    outMessage.classList.add("out-warning-message");
+    outMessage.innerHTML = `You entered ${select} on a Supper Contract with ${numbPoints} points`;
+  // } else if (numbPoints <= failBall) {
+  //   outTitle.classList.remove("d-none");
+  //   outTitle.classList.add("out-fail-title");
+  //   outTitle.innerHTML = "Sorry";
+  //   outMessage.classList.add("out-fail-message");
+  //   outMessage.innerHTML = `You entered ${select} on a Fail basis with ${numbPoints} points`;
+  // }
+
+
 
 });
